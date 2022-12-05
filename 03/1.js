d8865d8120d4f2b216b0e5b3978b00dae75e3ca1
brighter <bostjan.rihter@gmail.com>
2022-12-05 19:56:54 +0100

    day 3.1

---
+++ 03/1.js
@@ -0,0 +1,31 @@
+import { argv } from 'node:process'
+import { readFileSync } from 'node:fs'
+
+const [_, __, input] = argv
+
+const read = input => readFileSync(input, { encoding: 'ascii' })
+
+const duplicate = (line) => {
+  const m = line.length/2
+  const s1 = new Set(line.substr(0, m))
+  const s2 = new Set(line.substr(m))
+  return [...s1].filter(x => s2.has(x)).pop()
+}
+
+const score = (item) => {
+  const isLowercase = (item) => item.match(/[a-z]/) !== null
+
+  const code = item.charCodeAt(0)
+  return isLowercase(item) ? code - 96 : code - 38
+}
+
+const sum = (sum, value) => sum + value
+
+// prettier-ignore
+const result = read(input)
+  .split('\n')
+  .map(duplicate)
+  .map(score)
+  .reduce(sum, 0)
+
+console.log(result)
--- README.md
+++ README.md
@@ -7,4 +7,5 @@ node 01/1.js 01/input.txt
 node 01/2.js 01/input.txt
 node 02/1.js 02/input.txt
 node 02/2.js 02/input.txt
+node 03/1.js 03/input.txt
 ```
