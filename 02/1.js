c621e741a68b37c0f652b9b560c39a0480f4b3b6
brighter <bostjan.rihter@gmail.com>
2022-12-05 13:50:49 +0100

    nicer

--- 02/1.js
+++ 02/1.js
@@ -3,25 +3,17 @@ import { readFileSync } from 'node:fs'
 
 const [_, __, input] = argv
 
-// generate all possible outcomes of rps x rps (rps = rock, paper, scissors)
 const outcomes = [
   ['D', 'W', 'L'],
   ['L', 'D', 'W'],
   ['W', 'L', 'D']
 ]
 
-const scores = {
-  L: 0,
-  D: 3,
-  W: 6
-}
+const scores = { L: 0, D: 3, W: 6 }
 
-const read = input => {
-  return readFileSync(input, { encoding: 'ascii' })
-}
+const read = input => readFileSync(input, { encoding: 'ascii' })
 
 const parse = (row) => {
-  // transform letters to indexes
   const t = {
     'A': 0, 'B': 1, 'C': 2,
     'X': 0, 'Y': 1, 'Z': 2
@@ -30,13 +22,9 @@ const parse = (row) => {
   return row.split(' ').map(i => t[i])
 }
 
-const play = ([input, output]) => {
-  const outcome = outcomes[input][output]
-
-  let score = 0
-  score += output + 1 // add item score; fn(score) = item_index + 1
-  score += scores[outcome] // add outcome score
-  return score
+const play = ([a, b]) => {
+  const outcome = outcomes[a][b]
+  return b + 1 + scores[outcome]
 }
 
 const sum = (sum, item) => sum + item
--- 02/2.js
+++ 02/2.js
@@ -3,33 +3,23 @@ import { readFileSync } from 'node:fs'
 
 const [_, __, input] = argv
 
-// generate all possible outcomes of rps x rps (rps = rock, paper, scissors)
 const outcomes = [
   ['D', 'W', 'L'],
   ['L', 'D', 'W'],
   ['W', 'L', 'D']
 ]
 
-const scores = {
-  L: 0,
-  D: 3,
-  W: 6
-}
+const scores = { L: 0, D: 3, W: 6 }
 
-const read = input => {
-  return readFileSync(input, { encoding: 'ascii' })
-}
+const read = input => readFileSync(input, { encoding: 'ascii' })
 
 const parse = (row) => {
-  // transform letters to indexes
   const t = {
     'A': 0, 'B': 1, 'C': 2,
     'X': 'L', 'Y': 'D', 'Z': 'W'
   }
 
-  return row
-    .split(' ')
-    .map(i => t[i])
+  return row.split(' ').map(i => t[i])
 }
 
 const find = ([input, result]) => {
@@ -37,16 +27,12 @@ const find = ([input, result]) => {
   return [input, output]
 }
 
-const play = ([input, output]) => {
-  const outcome = outcomes[input][output]
-
-  let score = 0
-  score += output + 1 // add item score; fn(score) = item_index + 1
-  score += scores[outcome] // add outcome score
-  return score
+const play = ([a, b]) => {
+  const outcome = outcomes[a][b]
+  return b + 1 + scores[outcome]
 }
 
-const sum = (sum, item) => sum + item
+const sum = (sum, value) => sum + value
 
 const result = read(input)
   .split('\n')
