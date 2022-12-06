bf4392d22bffd1b4d86ed13f3676a565bf033a0d
brighter <bostjan.rihter@gmail.com>
2022-12-06 17:04:26 +0100

    day 5

--- README.md
+++ README.md
@@ -9,4 +9,8 @@ node day-02/1.js day-02/input.txt
 node day-02/2.js day-02/input.txt
 node day-03/1.js day-03/input.txt
 node day-03/2.js day-03/input.txt
+node day-04/1.js day-04/input.txt
+node day-04/2.js day-04/input.txt
+node day-05/1.js day-05/input.txt
+node day-05/2.js day-05/input.txt
 ```
---
+++ day-05/1.js
@@ -0,0 +1,65 @@
+import { argv } from 'node:process'
+import { readFileSync } from 'node:fs'
+
+const [_, __, input] = argv
+
+const read = input => readFileSync(input, { encoding: 'ascii' })
+
+const toStack = (file) => {
+  const raw = file
+    .split('\n')
+    .filter(l => !l.startsWith('move'))
+    .filter(l => l.length > 0)
+    .reverse()
+
+  const header = raw
+    .filter((l, index) => index === 0)
+    .pop()
+
+  const stack = header
+    .split(' ')
+    .filter(l => l.length > 0)
+    .map(x => [])
+
+  const parse = (stack, l) => {
+    for (let i = 0; i < stack.length; ++i) {
+      const crate = l.substring(i*4+1, i*4+2).trim()
+      if (crate.length === 0) continue
+      stack[i].push(crate)
+    }
+    return stack
+  }
+
+  return raw
+    .filter((l, index) => index > 0)
+    .reduce(parse, stack)
+}
+
+const move = (stack, op) => {
+  const [,quantity,from,to] = [...op.matchAll(/move (\d+) from (\d+) to (\d+)/g)].pop()
+
+  const mod = []
+  for(let i=0; i<quantity;i++) {
+    mod.push(stack[from-1].pop())
+  }
+
+  for(let i=0; i<quantity;i++) {
+    stack[to-1].push(mod[i])
+  }
+
+  return stack
+}
+
+const tail = (items) => items[items.length-1]
+
+const file = read(input)
+const stack = toStack(file)
+const result = file
+  .split('\n')
+  .filter(l => l.startsWith('move'))
+  .reduce(move, stack)
+  .map(tail)
+  .join('')
+
+  console.log(result)
+  
