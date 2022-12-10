8ebbf1cc304ae4928788d4aea5df2ca05bdefcbb
brighter <bostjan.rihter@gmail.com>
2022-12-10 13:40:48 +0100

    day 10

--- day-10/index.js
+++ day-10/index.js
@@ -10,8 +10,14 @@ const parse = (str) => {
   if (ins === 'addx') return [false, { ins, val: Number(val) }]
 }
 
-const execute = (state, op, cycle) => {
-  state.trace[cycle + 1] = (cycle + 1) * state.registers.x
+const execute = (state, op, i) => {
+  // during exec
+  const x = state.registers.x
+  const c = ([x-1,x,x+1].includes(i%40)) ? '#' : '.'
+  state.trace[i+1] = (i+1)*state.registers.x
+  state.buffer += c
+
+  // post exec
   if (op?.ins === 'addx') state.registers.x += op.val
   return state
 }
@@ -23,11 +29,19 @@ const state = read(input)
   .flatMap(parse)
   .reduce(execute, {
     registers: { x: 1 },
-    trace: [0]
+    trace: [0],
+    buffer: ''
   })
 
-const result = state.trace
+const result1 = state.trace
   .filter((v, cycle) => cycle % 40 === 20) // 20, 40, 80, ...
   .reduce(sum, 0)
 
-console.log(result)
+console.log(result1)
+
+const result2 = state.buffer
+  .split('')
+  .map((c, i) => (i+1)%40===0 ? c+'\n' : c) // 40 chars per line
+  .join('')
+
+console.log(result2)
